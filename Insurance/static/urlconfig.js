36a76d06bc9d085bab4d2748013d6bb1cdc08806
GuruAnkalagi <41356858+GuruAnkalagi@users.noreply.github.com>
2018-07-18 14:58:03 +0530

    Add files via upload

---
+++ Insurance/static/urlconfig.js
@@ -0,0 +1,8 @@
+export const aboutus_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/aboutus/fbeeefa9600346f48f24bb";
+export const slogan_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/slogan/fbeeefa9600346f48f24bb";
+export const logo_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/logo/fbeeefa9600346f48f24bb";
+export const contact_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/contactus/fbeeefa9600346f48f24bb";
+export const team_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/team/fbeeefa9600346f48f24bb";
+export const service_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/services/fbeeefa9600346f48f24bb";
+export const whyus_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/whyus/fbeeefa9600346f48f24bb";
+export const map_url = "http://ec2-34-210-179-250.us-west-2.compute.amazonaws.com:4567/customer/fbeeefa9600346f48f24bb";
